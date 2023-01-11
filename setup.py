fe64b3369d67aa2ba54e974144e3c3afe768e74e
Daniel McDonald <mcdonadt@colorado.edu>
2023-01-11 11:48:08 -0800

    Initial files

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+import versioneer
+
+setup(
+    name="q2-mislabeled",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages=find_packages(),
+    url="",
+    license="BSD-3-Clause",
+    description="Assess sample mislabelings",
+    entry_points={
+        "qiime2.plugins":
+        ["q2-mislabeled=q2_mislabeled.plugin_setup:plugin"]
+    },
+    package_data={
+        'q2_mislabeled.tests': [],
+        'q2_mislabeled': []
+    },
+    zip_safe=False,
+)
